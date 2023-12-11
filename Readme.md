b4df2e957b4f745bbfc1a89fa1b5593a18fbb00f
Essogbé <amrromuald234@gmail.com>
2023-12-11 19:48:41 +0100

    test

---
+++ Readme.md
@@ -0,0 +1,55 @@
+# Bank Management System
+
+Welcome to the Bank Management System, a beginner-friendly C++ project designed to help you understand basic concepts of object-oriented programming and simple console-based applications.
+
+## Table of Contents
+- [Introduction](#introduction)
+- [Features](#features)
+- [Getting Started](#getting-started)
+- [Usage](#usage)
+- [Contributing](#contributing)
+- [License](#license)
+
+## Introduction
+
+The Bank Management System is a console-based application implemented in C++. It provides a simple and interactive way to manage bank accounts, perform transactions, and display transaction history. This project serves as a great starting point for beginners to grasp fundamental concepts such as classes, objects, and basic input/output operations in C++.
+
+## Features
+
+1. **Create New Account:** Register a new bank account with a name, account number, and initial balance.
+2. **Show All Accounts:** View a list of all registered bank accounts with basic information.
+3. **Search Account:** Search for a specific account using the account number and display its details.
+4. **Deposit Money:** Add funds to a bank account and record the transaction.
+5. **Withdraw Money:** Withdraw funds from a bank account, provided there are sufficient funds, and record the transaction.
+6. **Display Transactions:** View a detailed history of transactions associated with a specific account.
+7. **Exit:** Quit the Bank Management System.
+
+## Getting Started
+
+To run the Bank Management System, follow these steps:
+
+1. **Clone the Repository:**
+   ```bash
+   git clone https://github.com/your_username/bank-management-system.git
+   cd bank-management-system
+   ```
+
+2. **Compile and Run:**
+   ```bash
+   g++ main.cpp -o bank_management_system
+   ./bank_management_system
+   ```
+
+## Usage
+
+Follow the on-screen menu to navigate through different options. Use numeric input to select your choice. The system allows you to create accounts, perform transactions, and view account details.
+
+## Contributing
+
+Contributions are welcome! If you have suggestions for improvements or new features, feel free to create an issue or submit a pull request.
+
+## License
+
+Feel free to use and modify the code for your educational and personal projects.
+
+Happy coding! 🚀
---
+++ main.cpp
@@ -0,0 +1,285 @@
+#include <iostream>
+#include <vector>
+#include <sstream>
+#include <ctime>
+#include <chrono>
+#include <iomanip>
+
+#ifdef _WIN32
+#define CLEAR_SCREEN "cls"
+#else
+#define CLEAR_SCREEN "clear"
+#endif
+
+
+using namespace std;
+
+class BankAccount{
+private:
+    string name;
+    int accountNum;
+    double balance;
+    vector <string> transactions ;
+public:
+    // Constructor
+    BankAccount(string n,int ac, double  bal){
+        name = n;
+        accountNum = ac;
+        balance = bal;
+    }
+    // Setter
+    void setName(string n) {
+        name = n;
+    }
+    void setBalance(int newBalance){
+        balance = newBalance;
+    }
+
+
+    //Getter
+    string getName(){
+        return name;
+    }
+    int getAccountNum(){
+        return accountNum;
+    }
+    double getBalance(){
+        return balance ;
+    }
+
+    //Other Methods
+    void savetransaction(string transactiontype,const double money){
+        ostringstream time_format;
+        ostringstream oss;
+
+       //Get actual time
+
+         //High time precision (only for C++ 17 or more)
+        auto currentTime = chrono::system_clock::now();
+        auto timePoint = chrono::system_clock::to_time_t(currentTime);
+        auto fractions = currentTime.time_since_epoch() % std::chrono::seconds(1);
+
+        tm* now = gmtime(&timePoint);
+
+
+        time_format << put_time(now,"%Y-%m-%d %H:%M:%S") <<'.'<<setw(3)<<setfill('O')<<fractions.count();
+
+
+
+        /*
+        time_t t=time(0);
+        tm* now= gmtime(&t);
+
+        time_format << put_time(now,"%Y-%m-%d %H:%M:%S");
+         */
+        string time = time_format.str();
+
+
+        oss << "Time: " << time << " | " <<"Transaction Type : " <<transactiontype << " | "<< "Money : "  << money<< " | " <<" Bal. : "<< balance ;
+
+        string transaction = oss.str();
+
+        //save
+        transactions.push_back(transaction);
+
+}
+
+    void displayTransactions(){
+
+        if (!transactions.empty()) {
+            cout << "\t\t**********************************Transactions Info**********************************";
+            cout << "\t\t\tAccount Number: " << accountNum << endl;
+            for (int i=0 ;i<transactions.size();i++) {
+
+                cout <<"\t\t\tno "<<i+1 <<" ==> "<< transactions[i]<<endl;
+            }
+            cout << "\t\t**************************************************************************************";
+        }
+        else{
+
+        cout << "No transactions Found";
+
+    }
+    }
+
+
+    void depositMoney(double money){
+        balance +=money;
+
+        cout << "Successful Deposit Operation  . New Balance: "<<balance<<endl;
+        this->savetransaction("DEPOSIT",money);
+    }
+
+    void withdrawMoney(double taken) {
+        if (taken <= balance) {
+            balance -= taken;
+            this->savetransaction("WITHDRAW",taken);
+            cout << "Successful WithDraw Operation  . New Balance: " << balance << endl;
+        }
+        else
+            cout << "Insufficient Balance....";
+}};
+
+
+class BankManagement {
+private:
+    vector<BankAccount> accounts;
+
+public:
+    void AddAccount(string name, int accountNum, double balance) {
+        accounts.push_back(BankAccount(name, accountNum, balance));
+    }
+
+    void ShowAccount(int i) {
+        cout << "Name : " << accounts[i].getName() << "     Account Number : " << accounts[i].getAccountNum()
+             << "  Balance : " << accounts[i].getBalance() << endl;
+        cout << "\t\t***************INFO***************" << endl;
+        cout << "\t\t\t♠ Name : " << accounts[i].getName() << endl;
+        cout << "\t\t\t♠ Account Number : " << accounts[i].getAccountNum() << endl;
+        cout << "\t\t\t♠ Balance : " << accounts[i].getBalance() << endl;
+        cout << "\t\t**********************************" << endl;
+    }
+
+    void ShowAllAccounts() {
+        cout << "\t\tAll Account Holders" << endl;
+        for (int i = 0; i < accounts.size(); i++)
+            ShowAccount(i);
+    }
+
+    int SearchAccount(int accountnum) {
+        for (int i = 0; i < accounts.size(); i++) {
+            if (accounts[i].getAccountNum() == accountnum)
+                return i;
+        }
+        return -1;
+    }
+
+    BankAccount *findAccount(int accountnum) {
+        if (this->SearchAccount(accountnum) >= 0) {
+            int found = this->SearchAccount(accountnum);
+            return &accounts[found];
+        }
+    }
+
+
+};
+
+
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    BankManagement bank;
+    int choice;
+    char option;
+    do{
+        system(CLEAR_SCREEN);
+        cout << "\t\t  Bank Management System" <<  endl;
+        cout << "\t\t\tMain Menu" << endl;
+        cout << "\t\t 1. Create New Account "<<endl;
+        cout << "\t\t 2. Show All Account "<<endl;
+        cout << "\t\t 3. Search Account "<< endl;
+        cout << "\t\t 4. Deposit  Money"<< endl;
+        cout << "\t\t 5. Withdraw Money" << endl;
+        cout << "\t\t 6. Display Transactions" << endl;
+        cout << "\t\t 7. Exit" << endl;
+        cout << "---------------------------------------------"<<endl;
+        cout << "\tEnter Your Choice : ";
+        cin >> choice;
+        switch (choice) {
+            case 1:
+            {
+                string name;
+                int accountNum;
+                double balance;
+                cout << "\t\tEnter Name :";
+                cin >> name;
+                cout << "\t\tEnter the account number : ";
+                cin >> accountNum;
+                cout << "\t\tEnter Initial Balance : ";
+                cin >> balance;
+                bank.AddAccount(name,accountNum,balance);
+                cout << "\t\t Account Created Succesfully....."<< endl;
+                break;
+            }
+            case 2:{
+                bank.ShowAllAccounts();
+                break;
+            }
+            case 3 : {
+                int num;
+                int found;
+
+                cout << "\t\tEnter the account number : ";
+                cin >>num;
+                found= bank.SearchAccount(num);
+                if (found >0)
+                    bank.ShowAccount(found);
+                else
+                    cout<< " Account not found ";
+                break;
+            }
+            case 4:
+            {
+                int num;
+                double money;
+                cout << "\t\tEnter the account number : ";
+                cin >>num;
+                BankAccount* account = bank.findAccount(num);
+                if (account !=NULL){
+                    cout<< "\t\tEnter the money to deposit : ";
+                    cin>> money;
+                    account->depositMoney(money);
+                }
+                else
+                    cout<< " Account not found ";
+                break;
+            }
+            case 5:
+            {
+                int num;
+                double money;
+
+                cout << "\t\tEnter your account number : ";
+                cin >> num;
+                BankAccount* account=bank.findAccount(num);
+                if (account !=NULL){
+                    cout << "Enter the money to withdraw";
+                    cin >> money;
+                    account->withdrawMoney(money);
+                }
+                else{
+                    cout <<" Account not Found";
+                }
+                break;
+            }
+            case 6:
+            {
+                int num;
+
+
+                cout << "\t\tEnter your account number : ";
+                cin >> num;
+                BankAccount* account=bank.findAccount(num);
+                if (account !=NULL){
+
+                    account->displayTransactions();
+                }
+                else{
+                    cout <<" Account not Found";
+                }
+                break;
+            }
+            case 7:
+            {
+            option = 'n';
+                break;
+            }
+        }
+
+        cout << "\t\t Do you want to continue [Y/N] ?";
+        cin >> option;
+
+    }while(option== 'y' || option =='Y');
+
+    return 0;
+}
